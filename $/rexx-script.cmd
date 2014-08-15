4c97ee5ba783054ab9fb7d6025c23363f0ebe3e2
rousseau <rousseau@3ae8c665-79e0-dc11-ae90-00e081727c95>
2014-08-15 16:36:09 +0000

    Test Commit 5: line endings -- conversion finished.
    Now .cmd and .kmk files are in the repo with crlf and svn will also
    check them out with crlf eventhough no svn properties are present.
    
    git-svn-id: http://svn.netlabs.org/repos/odin32/branches/swt@22074 3ae8c665-79e0-dc11-ae90-00e081727c95

---
+++ $/rexx-script.cmd
@@ -0,0 +1,7 @@
+/* Rexx */
+
+say '';
+say 'This script should now be in the repo with CRLF line endings.';
+say 'It should checkout with git-svn and native svn on eCS and Linux';
+say 'and retain the line endings.';
+say '';
--- $/rexx-with-CRLF.cmd
+++
@@ -1,6 +0,0 @@
-/* Rexx */
-
-say '';
-say 'This script was git-svn committed with CRLF line endings.';
-say 'When checked out, this should still be the case.';
-say '';
