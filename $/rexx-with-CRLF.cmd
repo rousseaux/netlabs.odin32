5ad36f30dc5dc79a4f14883422db8f6b95c85adb
rousseau <rousseau@3ae8c665-79e0-dc11-ae90-00e081727c95>
2014-08-15 09:05:14 +0000

    Test Commit 1: line endings.
    o rexx-with-CRLF.cmd
    o rexx-with-LF.cmd
    
    git-svn-id: http://svn.netlabs.org/repos/odin32/branches/swt@22070 3ae8c665-79e0-dc11-ae90-00e081727c95

---
+++ $/rexx-with-CRLF.cmd
@@ -0,0 +1,6 @@
+/* Rexx */
+
+say '';
+say 'This script was git-svn committed with CRLF line endings.';
+say 'When checked out, this should still be the case.';
+say '';
---
+++ $/rexx-with-LF.cmd
@@ -0,0 +1,8 @@
+/* Rexx */
+
+say '';
+say 'This script was git-committed with only LF line endings.';
+say 'When checked out with git-svn it should be converted to CRLF';
+say 'provided that the correct .gitattributes file is present.';
+say 'Behavior when checking out with svn will be tested next.';
+say '';
